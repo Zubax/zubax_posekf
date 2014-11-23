d34dbc376096f1a1d793ad696981132ba8a80fb0
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-11-23 19:12:39 +0300

    Paper

---
+++ paper/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SRC=main
+
+#rm -rf out &> /dev/null
+mkdir out &> /dev/null
+cp -fP *.bib out/ &> /dev/null
+
+pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+cd out
+biber $SRC
+cd ..
+pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+
+if [ -f out/$SRC.pdf ]
+then
+    (xdg-open out/$SRC.pdf &)
+fi
+
+# TODO: This is bad
+rm *.pdf*
